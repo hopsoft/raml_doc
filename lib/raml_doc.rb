5fb8102ac744762a42fd63d241d9b5ebf00618d8
Nathan Hopkins <natehop@gmail.com>
2016-05-25 09:26:20 -0400

    Set defaults for template & format

--- lib/raml_doc.rb
+++ lib/raml_doc.rb
@@ -4,7 +4,7 @@ require "raml_doc/version"
 require "raml_doc/view"
 
 module RamlDoc
-  def self.generate(file_path, template, format)
+  def self.generate(file_path, template="default", format="html")
     raml = Raml.parse_file(file_path)
     view = View.new(raml)
     view.render template, format
